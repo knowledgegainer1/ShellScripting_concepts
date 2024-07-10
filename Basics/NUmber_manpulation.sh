eaa7132c350ff90ae69950ea642ab618de335f88
avinashgottam <gottamavinash@gmail.com>
2024-07-11 00:33:39 +0530

    added number

--- Basics/NUmber_manpulation.sh
+++ Basics/NUmber_manpulation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-x=$1
-y=$2
-v=$3
-z=$(($x $3 $y))
-echo $z
+# x=$1
+# y=$2
+# v=$3
+# z=$(($x $3 $y))
+# echo $z
+
+exam=("apple" "sdc" "efw")
+echo "${exam[0]} ${exam[1]} ${exam[2]} "

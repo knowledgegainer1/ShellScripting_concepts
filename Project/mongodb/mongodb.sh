6b6ff7c9d4bc13b9a7455b202d26402d73544f55
avinashgottam <gottamavinash@gmail.com>
2024-07-11 20:08:50 +0530

    sef

--- Project/mongodb/mongodb.sh
+++ Project/mongodb/mongodb.sh
@@ -3,5 +3,9 @@
 id=$(id -u)
 date=$(date +%F-%T)
 folder_path="/tmp/$0_$date.log"
-
+if [ $id -ne 0 ]
+then
+ echo "Error: please login with sudo user and try"
+ exit 1
+fi
 echo "details saved to $folder_path"

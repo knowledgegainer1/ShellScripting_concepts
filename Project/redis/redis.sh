21ed582e370974a12421e00e8dfe259770b20f2b
avinashgottam <gottamavinash@gmail.com>
2024-07-12 20:36:22 +0530

    added  fi space

--- Project/redis/redis.sh
+++ Project/redis/redis.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-validate (){
+validate () {
     if [ $1 -ne 0 ]
     then
      echo " $2 ....is $R FAIED $N"
@@ -25,10 +25,13 @@ dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $path
 validate $? "installation is "
 
 dnf module enable redis:remi-6.2 -y  &>> $path
+validate $? "modile enable  is "
 
 dnf install redis -y  &>> $path
+validate $? "redis Instalaltion is "
 
 sed -i 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf   &>> $path
+validate $? "Ip change is "
 systemctl enable redis
 systemctl start redis
 netstat -lntp | head -n 3

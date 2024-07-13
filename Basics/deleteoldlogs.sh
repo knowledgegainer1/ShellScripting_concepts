95f458b17d2cb87aba04d85fc52169e32a31e9d0
avinashgottam <gottamavinash@gmail.com>
2024-07-13 15:58:01 +0530

    adec

--- Basics/deleteoldlogs.sh
+++ Basics/deleteoldlogs.sh
@@ -4,7 +4,7 @@
 
 source_dir="/tmp/logs_folder"
 files=$(find . -type f -mtime +14 -name "*.log")
-if [ -d $source_dir ]
+if [ ! -d $source_dir ]
 then
  echo "error folder foesnt exist so creating directoei $source_dir"
  mkdir $source_dir
